caf538825e972112b50e950ea1731c7ea4132d05
Romina Trazzi <romina.trazzi@posta.it>
2020-12-16 20:42:16 +0100

    Commit struttura html\css

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/* 
+
+Funzionalitá:
+La nostra todo list avrá alcune tasks di default predefinite
+L'utente puó inserire nuove tasks
+Cliccando sulla "X" l'utente puó cancellare una task
+Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+
+*/
+
+let root = new Vue ({
+    el = "#root",
+    data =
+    methods =
+
+
+
+
+})
