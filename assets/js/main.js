d3067d2c84dcd256d478e9076244b1844ecb0163
Romina Trazzi <romina.trazzi@posta.it>
2020-12-17 10:55:23 +0100

    Commit addElement

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,29 @@ La nostra todo list avrá alcune tasks di default predefinite
 L'utente puó inserire nuove tasks
 Cliccando sulla "X" l'utente puó cancellare una task
 Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
-Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il tasto Enter della tastiera.
 Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
 
 */
 
 let root = new Vue ({
-    el = "#root",
-    data =
-    methods =
+    el: "#root",
+    data: {
+        nuovoTask: "",
+        listaTask: ["Studiare JS", "Vedere Vue"],
+        immagine: "https://www.boolean.careers/images/misc/logo.png"
+    },
+
+    methods: {
+        addElement() {
+            this.listaTask.push(this.nuovoTask)
+            this.nuovoTask = ""
+        },
+
+        deleteElement() {
+            
+        }
+    } 
 
 
-
-
-})
+}); 
